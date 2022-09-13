d07bbe0022f59d637992b0e85dfe9348753a221d
JeongWooHyeon <jeong_wh@stoneb.jp>
2022-09-14 00:48:21 +0900

    Initial commit

---
+++ src/main/java/com/example/busanit403/BusanIt403Application.java
@@ -0,0 +1,13 @@
+package com.example.busanit403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanIt403Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusanIt403Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/busanit403/BusanIt403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busanit403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusanIt403ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
